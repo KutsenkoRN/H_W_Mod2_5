bbf02ad21648c6d091a5c31d871289c9caf5b7d8
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-07-29 13:41:01 +0300

    Add files via upload

---
+++ App.cs
@@ -0,0 +1,72 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_W_Mod2_5
+{
+    internal class App
+
+    {
+        private readonly IActions _action;
+        private readonly ILogger _logger;
+
+        public App(IActions action, ILogger logger)
+        {
+            _action = action;
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            var res = new Result();
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    int numberOfAction = new Random().Next(1, 4);
+                    switch (numberOfAction)
+                    {
+                        case 1:
+                            {
+                                res = _action.LogInfoActionExample();
+
+                                break;
+                            }
+                        case 2:
+                            {
+                                res = _action.LogWarningActionExample();
+
+
+                                break;
+                            }
+                        case 3:
+                            {
+                                res = _action.LogErrorActionExample();
+
+                                break;
+                            }
+                    }
+                }
+                catch (BusinessException e)
+                {
+                    Console.WriteLine(e);
+                    _logger.Warning($"Action got this custom Exception: {e.Message}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    _logger.Error($"Action failed by reason: {e.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("finally");
+                }
+            }
+        }
+    }
+}
---
+++ ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace H_W_Mod2_5;
+
+
+    internal interface ILogger
+{
+    void Log(LogType type, string message);
+
+    void Error(string message);
+    void Info(string message);
+    void Warning(string message);
+}
