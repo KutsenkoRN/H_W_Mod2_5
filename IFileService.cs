881e12ff086a964b1ce5f41393cb4f2259d96b17
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-08-05 14:37:48 +0300

    Add files via upload

---
+++ IFileService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace H_W_Mod2_5
+{
+    internal interface IFileService
+    {
+        void Save(string messages);
+    }
+       
+}
